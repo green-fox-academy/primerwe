d431df07b72668972a66b3d9043f10fadb31a4b2
primerwe <primerweblap@gmail.com>
2017-11-29 15:13:40 +0100

    it works

---
+++ week-02/day-3/13-quote_swap.py
@@ -0,0 +1,18 @@
+# Accidentally I messed up this quote from Richard Feynman.
+# Two words are out of place
+# Your task is to fix it by swapping the right words with code
+
+# Also, print the sentence to the output with spaces in between.
+
+#"What I cannot create, I do not understand"
+
+words = ["What", "I", "do", "create,", "I", "cannot", "not", "understand."]
+
+a, b = words.index("do"), words.index("cannot")
+words[b], words[a] = words[a], words[b]
+
+print(" ".join(words))
+
+
+
+
