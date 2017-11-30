43282ad926343dbf7c1610483b4d597c37476002
primerwe <primerweblap@gmail.com>
2017-11-30 17:40:46 +0100

    works

---
+++ week-02/day-5/anagram.py
@@ -0,0 +1,23 @@
+str1 = str(input("Enter the first word: "))
+str2 = str(input("Enter the second word: "))
+
+
+def is_anagram(str1,str2):
+    list1 = list(str1)
+    list2 = list(str2)
+
+    list1.sort()
+    list2.sort()
+
+    i = 0
+    isAnagrams = True
+
+    while i < len(str1) and isAnagrams:
+        if list1[i] == list2[i]:
+            i += 1
+        else:
+            isAnagrams = False
+
+    return isAnagrams
+
+print("The two words are anagram?:", is_anagram(str1,str2))
