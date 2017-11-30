5b535cdf1cc57c225217203f57ab7c7c08cba46b
primerwe <primerweblap@gmail.com>
2017-11-30 16:31:36 +0100

    finished

---
+++ week-02/day-4/03-substr.py
@@ -0,0 +1,28 @@
+# Find part of an integer
+#
+# -  Create a function that takes two strings as a parameter
+# -  Returns the starting index where the second one is starting in the first one
+# -  Returns `-1` if the second string is not in the first one
+#
+# ## Example
+# -  input: `"this is what I'm searching in"`, `"searching"`
+# -  output: `17`
+
+inputs = "this is what I'm searching in"
+char = "searching"
+
+def find_str(inputs, char):
+    i = 0
+
+    if char in inputs:
+        c = char[0]
+        for ch in inputs:
+            if ch == c:
+                if inputs[i:i+len(char)] == char:
+                    return i
+
+            i += 1
+
+    return -1
+
+print(find_str("this is what I'm searching in", "searching"))
