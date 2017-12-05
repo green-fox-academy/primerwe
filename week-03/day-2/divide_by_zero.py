d59ff4b2e0814958f336a3a6ce779d94eb538696
primerwe <primerweblap@gmail.com>
2017-12-05 09:43:06 +0100

    finished

---
+++ week-03/day-2/divide_by_zero.py
@@ -0,0 +1,15 @@
+# create a function that takes a number,
+# divides ten with it,
+# and prints the result.
+# it should print "fail" if the parameter is 0
+
+divisor = int(input("Enter a number: "))
+
+def divide():
+        try:
+            result = 10 / divisor
+            return(result)
+        except ZeroDivisionError:
+            return("Fail")
+            
+print(divide())
