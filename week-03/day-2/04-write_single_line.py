5c1a28f40df122dbe6f1005ae3f8a2bb70296ba4
primerwe <primerweblap@gmail.com>
2017-12-05 12:33:55 +0100

    works

---
+++ week-03/day-2/04-write_single_line.py
@@ -0,0 +1,17 @@
+# Open a file called "my-file.txt"
+# Write your name in it as a single line
+# If the program is unable to write the file,
+# then it should print an error message like: "Unable to write file: my-file.txt"
+
+file_name = input("Enter a filename: ")
+
+try:
+    my_file = open(file_name, "w")
+    my_file.write("Hello")
+    my_file.close()
+except Exception:
+    print("Unable to write file: ", file_name)
+
+my_file = open(file_name)
+s = my_file.read()
+print(s)
