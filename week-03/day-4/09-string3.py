3e2894eeff55cc490ff1a2c5647654cc4bfc6f5f
primerwe <primerweblap@gmail.com>
2017-12-07 15:04:15 +0100

    remove * after the last character

--- week-03/day-4/09-string3.py
+++ week-03/day-4/09-string3.py
@@ -6,6 +6,8 @@ string = input('Enter a string: ')
 def separate_chars(string):
     if string == '':
         return ''
+    elif string == string[::-1]:
+        return string[::-1]
     else:
         return string[0] + '*' + separate_chars(string[1:])
     
