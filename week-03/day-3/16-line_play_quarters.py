54f299549591f6393dc52a697ce410f3ca493a86
primerwe <primerweblap@gmail.com>
2017-12-09 17:52:02 +0100

    change background color to random color

--- week-03/day-3/16-line_play_quarters.py
+++ week-03/day-3/16-line_play_quarters.py
@@ -6,42 +6,34 @@ import random
 
 x = 0
 y = 0
-
+rand_color = str('#%06X' % random.randint(0,256**3-1))
 def half_lines():
     hline = canvas.create_line(0, 150, 300, 150, fill="black", width=1)
     vline = canvas.create_line(150, 0, 150, 300, fill="black", width=1)
 def line_play_quarter(x,y):
     for i in range(1,8):
-        rand_color = ('#%06X' % random.randint(0,256**3-1))
-        lines = canvas.create_line(0, y+(i*20), x+(i*20), 150, fill=rand_color, width=1)
+        lines = canvas.create_line(0, y+(i*20), x+(i*20), 150, fill="red", width=1)
     for i in range(1,8):
-        rand_color = ('#%06X' % random.randint(0,256**3-1))
-        lines = canvas.create_line(x+(i*20), 0, 150, y+(i*20), fill=rand_color, width=1)
+        lines = canvas.create_line(x+(i*20), 0, 150, y+(i*20), fill="blue", width=1)
         
     for i in range(1,8):
-        rand_color = ('#%06X' % random.randint(0,256**3-1))
-        lines = canvas.create_line(150, y+(i*20), 150+x+(i*20), 150, fill=rand_color, width=1)
+        lines = canvas.create_line(150, y+(i*20), 150+x+(i*20), 150, fill="green", width=1)
     for i in range(1,8):
-        rand_color = ('#%06X' % random.randint(0,256**3-1))
-        lines = canvas.create_line(150+x+(i*20), 0, 300, y+(i*20), fill=rand_color, width=1)
+        lines = canvas.create_line(150+x+(i*20), 0, 300, y+(i*20), fill="purple", width=1)
         
     for i in range(1,8):
-        rand_color = ('#%06X' % random.randint(0,256**3-1))
-        lines = canvas.create_line(0, 150+y+(i*20), x+(i*20), 300, fill=rand_color, width=1)
+        lines = canvas.create_line(0, 150+y+(i*20), x+(i*20), 300, fill="blue", width=1)
     for i in range(1,8):
-        rand_color = ('#%06X' % random.randint(0,256**3-1))
-        lines = canvas.create_line(x+(i*20), 150, 150, 150+y+(i*20), fill=rand_color, width=1)
+        lines = canvas.create_line(x+(i*20), 150, 150, 150+y+(i*20), fill="red", width=1)
         
     for i in range(1,8):
-        rand_color = ('#%06X' % random.randint(0,256**3-1))
-        lines = canvas.create_line(150, 150+y+(i*20), 150+x+(i*20), 300, fill=rand_color, width=1)
+        lines = canvas.create_line(150, 150+y+(i*20), 150+x+(i*20), 300, fill="purple", width=1)
     for i in range(1,8):
-        rand_color = ('#%06X' % random.randint(0,256**3-1))
-        lines = canvas.create_line(150+x+(i*20), 150, 300, 150+y+(i*20), fill=rand_color, width=1)
+        lines = canvas.create_line(150+x+(i*20), 150, 300, 150+y+(i*20), fill="green", width=1)
     
 root = Tk()
 
-canvas = Canvas(root, width='300', height='300', bg='#a0a0a0')
+canvas = Canvas(root, width='300', height='300', bg=rand_color)
 canvas.pack()
 
 half_lines()
