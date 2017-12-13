ab20f445f792356638b8090f51cea230f1338ff8
primerwe <primerweblap@gmail.com>
2017-12-13 16:32:36 +0100

    completed

---
+++ week-04/day-3/extend.py
@@ -0,0 +1,34 @@
+# Adds a and b, returns as result
+def add(a, b):
+    return a + b
+
+# Returns the highest value from the three given params
+def max_of_three(a, b, c):
+    if a > b and a > c:
+        return a
+    elif b > a and b > c:
+        return b
+    else:
+        return c
+
+# Returns the median value of a list given as param
+def median(pool):
+    index = (len(sorted(pool)) - 1) // 2
+    if (len(pool) % 2):
+        return sorted(pool)[index]
+    else:
+        return (sorted(pool)[index] + sorted(pool)[index + 1]) / 2.0
+    
+    #return pool[int((len(pool) - 1) / 2)]
+
+# Returns true if the param is a vovel
+def is_vovel(char):
+    return char.lower() in 'aeiouéáőűöüóí'
+
+# Create a method that translates hungarian into the teve language
+def translate(hungarian):
+    teve = hungarian
+    for char in teve:
+        if is_vovel(char):
+            teve = (char+'v'+char).join(teve.split(char))
+    return teve
