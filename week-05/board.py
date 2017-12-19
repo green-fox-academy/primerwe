7c485e3f583d139d35d89c6d07c723298462c11b
primerwe <primerweblap@gmail.com>
2017-12-19 14:47:01 +0100

    refact(board): move mapdrawing related things to Board class

--- week-05/board.py
+++ week-05/board.py
@@ -1,6 +1,13 @@
-class Area(object):
+from tkinter import *
+
+class Board(object):
     #wall == 1, floor == 0
     def __init__(self):
+        self.floor = PhotoImage(file='elements/floor.png')
+        self.wall = PhotoImage(file='elements/wall.png')
+        self.tile = 70
+        self.x = 0
+        self.y = 0
         self.tiles = [
             [0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
             [0, 0, 0, 1, 0, 1, 0, 1, 1, 0],
@@ -14,10 +21,21 @@ class Area(object):
             [0, 1, 0, 0, 1, 1, 0, 0, 0, 1]
         ]
 
+        def draw_map(self, canvas):
+        for row in range(len(self.tiles)):
+            for col in range(len(self.tiles[i])):
+                if self.tiles[row][col] == 0:
+                    image = self.floor
+                else:
+                    image = self.wall
+                canvas.create_image(col * self.tile, row * self.tile, anchor=NW, image=image)
+        
+        
+        
 #    def is_wall(self, x, y):
 #        self.tiles[y][x] == 1
 #    
-#    def is_floor(self, x, y):
-#        self.tiles[y][x] == 0
+    def is_floor(self, x, y):
+        self.tiles[y][x] == 0
 #    
 #feature: generáljon 10db 0/1 random számot a listába 10szer és úgy rajzolja a térképet
