1daebf2c62064c8c79a0043bb96b55c360aca7e8
Fruzsina Kormendi <primerweblap@gmail.com>
2018-01-09 14:18:54 +0100

    exercise 10 done with built-in reverse() function

--- week-07/day-2/functions_and_arrays/10-swap-elements.js
+++ week-07/day-2/functions_and_arrays/10-swap-elements.js
@@ -2,6 +2,7 @@
 // - Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
 // - Swap the first and the third element of `abc`
 
+//swap with the hard way
 let abc = ["Arthur", "Boe", "Chloe"];
 
 let x = abc.pop();
@@ -11,3 +12,11 @@ abc.push(y);
 abc.unshift(x);
 
 console.log(abc);
+
+//swap with reverse()
+
+let abc2 = ["Arthur", "Boe", "Chloe"];
+
+abc2.reverse();
+
+console.log(abc2);
