7874a4fe1de8b8d7edb2692a426ee14858283e4d
Fruzsina Kormendi <primerweblap@gmail.com>
2018-01-09 14:42:04 +0100

    finished exercise 18

---
+++ week-07/day-2/functions_and_arrays/18-diagonal-matrix.js
@@ -0,0 +1,29 @@
+'use strict';
+// - Create (dynamically*) a two dimensional list
+//   with the following matrix**. Use a loop!
+//
+//   0 0 0 1
+//   0 0 1 0
+//   0 1 0 0
+//   1 0 0 0
+//
+// - Print this two dimensional list to the console
+//
+// * size should depend on a variable
+// ** Relax, a matrix is just like an array
+
+let size = 4;
+let matrix = [];
+
+for (let i = 0; i < size; i++) {
+    matrix[i] = [0];
+    for (let j = 0; j < size; j++) {
+        if (i + j === size - 1) {
+            matrix[i][j] = [1];
+        } else {
+            matrix[i][j] = [0];
+        }
+    }
+}
+
+console.log(matrix);
