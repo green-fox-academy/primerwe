80af16c0ec3a5f991f173bae55a4396257699b2c
Fruzsina Kormendi <primerweblap@gmail.com>
2018-01-09 16:10:36 +0100

    finished exercise 02

---
+++ week-07/day-2/arrays/02-matchmaking.js
@@ -0,0 +1,13 @@
+'use strict';
+// Join the two array by matching one girl with one boy in the order array
+// Exepected output: ["Eve", "Joe", "Ashley", "Fred"...]
+
+var girls = ["Eve","Ashley","Bözsi","Kat","Jane"];
+var boys = ["Joe","Fred","Béla","Todd","Neef","Jeff"];
+var order = [];
+
+for (let i = 0; i < boys.length; i++) {
+    order.push(girls[i], boys[i]);
+}
+
+console.log(order);
