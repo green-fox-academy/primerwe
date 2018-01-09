6a63f01ed68c2acd0872ffaf3ea55a0a977634ce
Fruzsina Kormendi <primerweblap@gmail.com>
2018-01-09 13:38:22 +0100

    finished exercise 12

---
+++ week-07/day-2/functions_and_arrays/12-change-element.js
@@ -0,0 +1,10 @@
+'use strict';
+// - Create an array named `s` with the following content: `[1, 2, 3, 8, 5, 6]`
+// - Change the 8 to 4 with the `.map` method 
+// - Print the fourth element as a test
+
+let s = [1, 2, 3, 8, 5, 6];
+
+const map1 = s.map(s => s/2, s[3]);
+
+console.log(s);
