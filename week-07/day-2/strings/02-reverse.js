a67a1efd6a32b5db7ffd8963bb0e1211950a328b
Fruzsina Kormendi <primerweblap@gmail.com>
2018-01-09 14:53:18 +0100

    finished exercise 02

---
+++ week-07/day-2/strings/02-reverse.js
@@ -0,0 +1,8 @@
+'use strict';
+// Reverse the string!
+
+var reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI"
+
+let reversedStr = reversed.split("").reverse().join("");
+
+console.log(reversedStr);
