6537b3fc7f09a3569508746c45ff2327e918f16d
Fruzsina Kormendi <primerweblap@gmail.com>
2018-01-11 16:02:06 +0100

    feat(gallery): add loop through thumbnails

--- week-07/day-4/js/main.js
+++ week-07/day-4/js/main.js
@@ -15,7 +15,7 @@ function currentSlide(n) {
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("slides");
-//    var dots = document.getElementsByClassName("dot");
+    var thumb = document.getElementsByClassName("style");
     if (n > slides.length) {
         slideIndex = 1;
     }
@@ -25,9 +25,9 @@ function showSlides(n) {
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-//    for (i = 0; i < dots.length; i++) {
-//        dots[i].className = dots[i].className.replace(" active", "");
-//    }
+    for (i = 0; i < thumb.length; i++) {
+        thumb[i].className = thumb[i].className.replace(" active", "");
+    }
     slides[slideIndex - 1].style.display = "block";
-//    dots[slideIndex - 1].className += " active";
+    thumb[slideIndex - 1].className += " active";
 }
