7f359e1a0eca0246c0059b19ffc99db34ea22ce7
Fruzsina Kormendi <primerweblap@gmail.com>
2018-01-11 13:09:34 +0100

    feat(gallery): slideshow work

--- week-07/day-4/js/main.js
+++ week-07/day-4/js/main.js
@@ -1,1 +1,33 @@
 'use strict'
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function changeSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+//thumbnails
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slides");
+//    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+//    for (i = 0; i < dots.length; i++) {
+//        dots[i].className = dots[i].className.replace(" active", "");
+//    }
+    slides[slideIndex - 1].style.display = "block";
+//    dots[slideIndex - 1].className += " active";
+}
