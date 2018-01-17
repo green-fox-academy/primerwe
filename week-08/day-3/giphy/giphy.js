6719441d2f631567e88e03ca31e304f03346f710
Fruzsina Kormendi <primerweblap@gmail.com>
2018-01-17 13:24:58 +0100

    feat(giphy): try to improve, add a search button to search random stuff

--- week-08/day-3/giphy/giphy.js
+++ week-08/day-3/giphy/giphy.js
@@ -1,6 +1,10 @@
 'use strict';
 
-function getGiphies() {
+let btn = document.querySelector('form>button#btn');
+let searchBox = document.querySelector('form>input#searchBox');
+
+//function getGiphies() {
+btn.addEventListener('click', function () {
     let xhr = new XMLHttpRequest();
     xhr.open('GET', 'http://api.giphy.com/v1/gifs/search?api_key=2NaHf0oqvaBflMtJ9Ya71Xm218wJ9do2&q=cat&limit=16&offset=0&rating=G&lang=en', true);
     xhr.onload = function () {
@@ -12,12 +16,18 @@ function getGiphies() {
     };
     xhr.send();
 
-};
+});
 
 function staticGiphies(item) {
+    let imageHTML = document.querySelector('ul#images');
     let image = document.createElement('img');
+    
     image.src = item.images['fixed_height_still'].url;
     document.body.appendChild(image);
-}
+    
+    image.addEventListener('click', function () {
+        image.src = item.images['fixed_height'].url;
+    });
+};
 
-getGiphies();
+//getGiphies();
