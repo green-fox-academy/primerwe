6480214857c6c9d88c3941020e12862b39840721
Fruzsina Kormendi <primerweblap@gmail.com>
2018-01-17 13:48:29 +0100

    feat(giphy): add search textcontent to the url, but not working :(

--- week-08/day-3/giphy/giphy.js
+++ week-08/day-3/giphy/giphy.js
@@ -6,7 +6,7 @@ let searchBox = document.querySelector('form>input#searchBox');
 //function getGiphies() {
 btn.addEventListener('click', function () {
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://api.giphy.com/v1/gifs/search?api_key=2NaHf0oqvaBflMtJ9Ya71Xm218wJ9do2&q=cat&limit=16&offset=0&rating=G&lang=en', true);
+    xhr.open('GET', 'http://api.giphy.com/v1/gifs/search?api_key=2NaHf0oqvaBflMtJ9Ya71Xm218wJ9do2&q=' + searchBox.textContent + '&limit=16&offset=0&rating=G&lang=en', true);
     xhr.onload = function () {
         let content = JSON.parse(xhr.responseText).data;
         content.forEach(function(item) {
