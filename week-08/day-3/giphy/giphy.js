5072c5cc9351d40ee67bb714216df0f3a14243c6
Fruzsina Kormendi <primerweblap@gmail.com>
2018-01-17 12:38:58 +0100

    feat(giphy): give back 16 still image workin progress

--- week-08/day-3/giphy/giphy.js
+++ week-08/day-3/giphy/giphy.js
@@ -1,16 +1,23 @@
 'use strict';
 
-let xhr = new XMLHttpRequest();
+function getGiphies() {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://api.giphy.com/v1/gifs/search?api_key=2NaHf0oqvaBflMtJ9Ya71Xm218wJ9do2&q=cat&limit=16&offset=0&rating=G&lang=en', true);
+    xhr.onload = function () {
+        let content = JSON.parse(xhr.responseText).data;
+        content.forEach(function(item) {
+            staticGiphies(item);
+        });
+        console.log(content);
+    };
+    xhr.send();
 
-xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-        let content = JSON.parse(xhr.responseText);
-        console.log(content.data.images.original.url);
-        let gifHTML = '<img src=" ' + content.data.images.original.url + ' ">'; //: img HTML tag dinamikus megkonstruálása
-        document.getElementById('ajax').innerHTML = gifHTML;
-    }
 };
 
-xhr.open('GET', 'http://api.giphy.com/v1/gifs/translate?api_key=2NaHf0oqvaBflMtJ9Ya71Xm218wJ9do2&s=cat&limit=16&offset=0&rating=G&lang=en', true);
+function staticGiphies(item) {
+    let image = document.createElement('img');
+    image.src = item.images['fixed_height_still'].url;
+    document.body.appendChild(image);
+}
 
-xhr.send();
+getGiphies();
