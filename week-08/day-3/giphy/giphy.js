fcf7ec3556252ab68e222687a07666d83803be5b
Fruzsina Kormendi <primerweblap@gmail.com>
2018-01-17 15:46:54 +0100

    mod(giphy): xhr.send() gives an error message

--- week-08/day-3/giphy/giphy.js
+++ week-08/day-3/giphy/giphy.js
@@ -5,9 +5,9 @@ let btn = document.querySelector('button#btn');
 
 //function getGiphies() {
 btn.addEventListener('click', function () {
-    let searchBox = document.querySelector('input#searchBox');
+    let searchBox = document.querySelector('input#searchBox').value;
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://api.giphy.com/v1/gifs/search?api_key=2NaHf0oqvaBflMtJ9Ya71Xm218wJ9do2&q=' + searchBox.textContent + '&limit=16&offset=0&rating=G&lang=en', true);
+    xhr.open('GET', 'http://api.giphy.com/v1/gifs/search?api_key=2NaHf0oqvaBflMtJ9Ya71Xm218wJ9do2&q=' + searchBox + '&limit=16&offset=0&rating=G&lang=en', true);
     xhr.onload = function () {
         let content = JSON.parse(xhr.responseText).data;
         content.forEach(function(item) {
@@ -23,7 +23,7 @@ function staticGiphies(item) {
     let image = document.createElement('img');
     
     image.src = item.images['fixed_height_still'].url;
-    document.body.appendChild(image);
+    document.imageHTML.appendChild(image);
     
     image.addEventListener('click', function () {
         image.src = item.images['fixed_height'].url;
