f10e051b7acf32162d1458e726a03c4951900ee4
Fruzsina Kormendi <primerweblap@gmail.com>
2018-01-17 12:24:11 +0100

    feat(giphy): workin progress - give back only one giphy

---
+++ week-08/day-3/giphy/giphy.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+        let content = JSON.parse(xhr.responseText);
+        console.log(content.data.images.original.url);
+        let gifHTML = '<img src=" ' + content.data.images.original.url + ' ">'; //: img HTML tag dinamikus megkonstruálása
+        document.getElementById('ajax').innerHTML = gifHTML;
+    }
+};
+
+xhr.open('GET', 'http://api.giphy.com/v1/gifs/translate?api_key=2NaHf0oqvaBflMtJ9Ya71Xm218wJ9do2&s=cat&limit=16&offset=0&rating=G&lang=en', true);
+
+xhr.send();
