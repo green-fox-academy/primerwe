cc8cb60a864ba29e2d05e88417af41ad31260b75
Fruzsina Kormendi <primerweblap@gmail.com>
2018-01-21 22:16:27 +0100

    fix(submit): still cannot post, get some error from the html

--- week-08/day-4/js/submit-post.js
+++ week-08/day-4/js/submit-post.js
@@ -1,41 +1,29 @@
 'use strict';
 
-let apiUrl = 'https://time-radish.glitch.me';
-let submitButton = document.querySelector('button.btn');
-submitButton.addEventListener('click', submit);
-
-function postRequest(request) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', apiUrl, true);
-    xhr.setRequestHeader('Accept', 'application/json');
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.setRequestHeader('Username', 'betterLateThanNever');
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            console.log('ready state = done');
-            if (xhr.status === 200) {
-                console.log('status = OK');
-                let request = xhr.responseText;
-            }
-        }
-    }
-    request = JSON.stringify(request);
-    xhr.send(request)
-};
-
-function visualizePost() {
-    let postText = document.querySelector('#title').value;
-    let postUrl = document.querySelector('#url').value;
-    
-    let sendForPost = new Object();
-    sendForPost.title = String(postText);
-    sendForPost.url = String(postUrl);
-    sendForPost.owner = "betterLateThanNever";
-    
-    let dataToSend = JSON.stringify(sendForPost);
-    console.log(dataToSend);
-}
+document.addEventListener('DOMContentLoaded', submit, false);
 
 function submit() {
-    postRequest(visualizePost());
+    let submitButton = document.querySelector('.submit_button');
+    submitButton.addEventListener('click', getPosts, true);
+};
+
+function getPosts() {
+    let title = document.querySelector('#post_title');
+    let url = document.querySelector('#post_url');
+    title.value === '' || url.value === '' ? alert('Required!') : formatTexts(title.value, url.value);
+};
+
+function formatTexts(title, url, owner){
+    submitPosts(`{ "title": "${title}", "url": "${url}"}, "owner": 'betterLateThanNever'`);
+};
+
+function submitPosts(message) {
+    let link = 'https://time-radish.glitch.me/posts';
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', link);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.onreadystatechange = function() {
+        xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200 ? console.log('Ready!') : console.log('Failed!');
+    }
+    xhr.send(message);
 }
