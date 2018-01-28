184f0b591d783ed2685861934dc19262054fe7de
Fruzsina Kormendi <primerweblap@gmail.com>
2018-01-28 17:00:42 +0100

    feat(reddit-backend): exercise started with local json data - work in progress

--- week-09/day-5/reddit-backend.js
+++ week-09/day-5/reddit-backend.js
@@ -11,45 +11,57 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.urlencoded());
 
+/*const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '123456',
+  database: 'bookstore'
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connecteion established!');
+});*/
+
 let posts = [
   {
-    "id": 25,
-    "title": "Dear JavaScript",
-    "url": "http://9gag.com",
-    "timestamp": 14943395,
-    "score": 793,
-    "owner": null,
-    "vote": 1
+  "id": 25,
+  "title": "Dear JavaScript",
+  "url": "http://9gag.com",
+  "timestamp": 14943395,
+  "score": 793,
+  "owner": null,
+  "vote": 1
 },
-  {
-    "id": 74,
-    "title": "Crockford",
-    "url": "http://9gag.com",
-    "timestamp": 14941384,
-    "score": 567,
-    "owner": "kristof4",
-    "vote": -1
+{
+  "id": 74,
+  "title": "Crockford",
+  "url": "http://9gag.com",
+  "timestamp": 14941384,
+  "score": 567,
+  "owner": "kristof4",
+  "vote": -1
 }];
 
-app.get('/posts', function (req, res) {
-  res.json({
-    "posts": posts,
-  });
+app.get('/posts', function(req, res) {
+    res.json({
+        "posts": posts,
+    });
 });
 
-app.put('/posts/:id/upvote', function (req, res) {
-  posts.forEach(item => item.id === parseInt(req.params.id, 10) ? item.score++ : item.score);
-  res.json();
+app.put('/posts/:id/upvote', function(req, res) {
+    posts.forEach(item => item.id === parseInt(req.params.id, 10) ? item.score++ : item.score);
+    res.json();
 });
 
-app.put('/posts/:id/downvote', function (req, res) {
-  posts.forEach(item => item.id === parseInt(req.params.id, 10) ? item.score-- : item.score);
-  res.json();
+app.put('/posts/:id/downvote', function(req, res) {
+    posts.forEach(item => item.id === parseInt(req.params.id, 10) ? item.score-- : item.score);
+    res.json();
 });
 
-app.delete('posts/:id', function (req, res) {
-  let index = 0;
-  posts.forEach(item => item.id === parseInt(req.params.id, 10) ? posts.splice(index, 1) : index++);
+app.delete('posts/:id', function(req, res) {
+    let index = 0;
+    posts.forEach(item => item.id === parseInt(req.params.id, 10) ? posts.splice(index, 1): index++);
 });
 
 app.post('/posts', function (req, res) {
@@ -58,6 +70,3 @@ app.post('/posts', function (req, res) {
   posts.push(req.body);
 });
 
-app.listen(port, function () {
-  console.log("The app is running at localhost: " + port);
-});
