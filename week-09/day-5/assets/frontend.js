b2ebb8cad216b5f3f2962aaa3d9f553a25f255d0
Fruzsina Kormendi <primerweblap@gmail.com>
2018-01-30 16:30:29 +0100

    feat(bookstore): finish exercise

---
+++ week-09/day-5/assets/frontend.js
@@ -0,0 +1,83 @@
+'use strict';
+
+let url = 'http://localhost:8080/books';
+
+function connect(method, url, callback) {
+  let xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.setRequestHeader("Content-type", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+      let request = JSON.parse(xhr.response);
+      callback(request);
+    }
+    /*else {
+         console.log('Connection failed!');
+       }*/
+  }
+  xhr.send();
+};
+
+function createTable(result) {
+  let message = document.querySelector('section#message');
+  let table = document.querySelector('table');
+  if (!result.books.length) {
+    table.innerHTML = "";
+    message.innerHTML = `<h2>No Match</h2>`;
+  } else {
+    message.innerHTML = "";
+    table.innerHTML =
+      `<tr>
+        <th>Title</th>
+        <th>Author</th>
+        <th>Category</th>
+        <th>Publisher</th>
+        <th>Price</th>
+      </tr>`
+    result.books.forEach(function (item) {
+      const fill =
+        `<tr>
+        <td>${item.title}</td>
+        <td>${item.author}</td>
+        <td>${item.category}</td>
+        <td>${item.publisher}</td>
+        <td>${item.price}</td>
+      </tr>`
+      table.innerHTML += fill;
+    })
+  }
+};
+
+let category = document.querySelector('#category');
+let publisher = document.querySelector('#publisher');
+let priceLo = document.querySelector('#price_lo');
+let priceHi = document.querySelector('#price_hi');
+let button = document.querySelector('button.search_button');
+
+function clickButton() {
+  button.addEventListener('click', function () {
+    let link = url + '?';
+    if (category.value.length) {
+      link += `category=${category.value}`;
+    }
+    if (publisher.value.length) {
+      link += `publisher=${publisher.value}`;
+    }
+    if (priceLo.value.length) {
+      link += `priceLo=${priceLo.value}`;
+    }
+    if (priceHi.value.length) {
+      link += `priceHi=${priceHi.value}`;
+    }
+    link = link.replace('?&', '?');
+    connect('GET', link, createTable);
+  })
+};
+
+function start() {
+  connect('GET', url, createTable);
+  clickButton();
+}
+
+start();
--- week-09/day-5/bookstore.js
+++ week-09/day-5/bookstore.js
@@ -2,8 +2,14 @@
 
 const mysql = require('mysql');
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const port = 8080;
 const app = express();
+
+app.use('/assets', express.static('assets'));
+app.use(bodyParser.json());
+app.use(cors());
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -12,22 +18,55 @@ const connection = mysql.createConnection({
   database: 'bookstore'
 });
 
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Connecteion established!');
+function connecToMySql() {
+  connection.connect((err) => {
+    if (err) {
+      console.log("Connection failed!");
+      return;
+    };
+    console.log("Connecteion established!");
+  });
+};
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/', function (req, res) {
-  connection.query('SELECT book_name FROM book_mast;', function (err, rows) {
-    if (err) {
-      console.log(err.toString());
-      res.status(500).send('Database error');
-      return;
+app.get('/books', function (req, res) {
+  let data = [];
+  let queryString = `
+    SELECT book_name, aut_name, cate_descrip, pub_name, book_price 
+    FROM book_mast 
+    INNER JOIN author ON book_mast.aut_id=author.aut_id 
+    INNER JOIN category ON book_mast.cate_id=category.cate_id 
+    INNER JOIN publisher ON book_mast.pub_id=publisher.pub_id
+  `;
+  if (req.query.category || req.query.publisher || req.query.priceLo || req.query.priceHi) {
+    queryString += ' WHERE ';
+    if (req.query.category) {
+      queryString += `cate_descrip LIKE '%${req.query.category}%'`;
     }
-    res.send(rows);
+    if (req.query.publisher) {
+      queryString += ` AND pub_name LIKE '%${req.query.publisher}%'`;
+    }
+    if (req.query.priceLo) {
+      queryString += ` AND book_price >= '${req.query.priceLo}'`;
+    }
+    if (req.query.priceHi) {
+      queryString += ` AND book_price <= '${req.query.priceHi}'`;
+    }
+    queryString = queryString.replace('WHERE  AND', 'WHERE');
+  }
+  
+  connection.query(queryString, function(err, result, fileds) {
+      result.forEach(function(element){
+        data.push({'title': element.book_name, 'author': element.aut_name, 'category': element.cate_descrip, 'publisher': element.pub_name, 'price': element.book_price})
+      });
+      res.send({'books': data});
   });
 });
 
+connecToMySql();
 app.listen(port, function () {
   console.log("The app is running at localhost: " + port);
 });
